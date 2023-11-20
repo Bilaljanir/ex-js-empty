bcddc00d125bf2ea3f47593d9cc0dd2334d32745
Bilaljanir <bilaljanir@gmail.com>
2023-11-20 16:10:06 +0100

    add function mouse and hoverFocusAndBlur

--- src/events/movements.js
+++ src/events/movements.js
@@ -13,7 +13,6 @@ export function mouseMovements() {
     document.addEventListener('mousemove', updateMousePosition);
 }
 
-
 /**
  * On the page, you have an input with the id "focus-me".
  * You need to add three behaviors to this input.
@@ -24,9 +23,33 @@ export function mouseMovements() {
  * Third, when you loose focus of the field, you need to reset the border color to the default one.
  */
 export function hoverFocusAndBlur() {
-  //
-}
+    const inputElement = document.getElementById('focus-me');
+    const labelElement = inputElement.previousElementSibling;
+    let usedColors = []; // To keep track of used colors
+    const getRandomColor = () => {
+        let color;
+        do {
+            color = '#' + Math.floor(Math.random() * 16777215).toString(16);
+        } while (usedColors.includes(color));
+        usedColors.push(color);
+        return color;
+    };
 
+    inputElement.addEventListener('mouseover', () => {
+        labelElement.textContent = "Yes, you hover me !";
+    });
+
+    inputElement.addEventListener('mouseout', () => {
+        labelElement.textContent = ""; // Reset the label text
+    });
+
+    inputElement.addEventListener('focus', () => {
+        inputElement.style.borderColor = getRandomColor();
+    });
+    inputElement.addEventListener('blur', () => {
+        inputElement.style.borderColor = ''; // Reset to default color
+    });
+}
 /**
  * On the same input from the previous exercise, you need to add a new behavior :
  * Now, each new letter on the input should randomly change the default color of the input border.
@@ -36,5 +59,18 @@ export function hoverFocusAndBlur() {
  * Take the opportunity to also apply this colour to the text of the 2 input labels.
  */
 export function changesOnInputEvents() {
-  //
+    const inputElement = document.getElementById('focus-me');
+    const labelElements = document.querySelectorAll('label[for="focus-me"]');
+    let defaultBorderColor = ''; // To keep track of the default border color
+    const getRandomColor = () => '#' + Math.floor(Math.random() * 16777215).toString(16);
+    const updateDefaultColor = () => {
+        defaultBorderColor = getRandomColor();
+        labelElements.forEach(label => label.style.color = defaultBorderColor);
+    };
+    inputElement.addEventListener('input', () => {
+        updateDefaultColor();
+    });
+    inputElement.addEventListener('blur', () => {
+        inputElement.style.borderColor = defaultBorderColor;
+    });
 }
